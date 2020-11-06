e9fe9ce3d1098eedeb45cb7ab5fa9d4f4d73f160
NakoMegAswa <72852456+NakoMegAswa@users.noreply.github.com>
2020-11-06 23:49:19 +0800

    Add files via upload

---
+++ Assets/TestScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text movementText;
+
+    private Rigidbody rb;
+    private int count;
+    private int Movement;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "SpaceBar : 0";
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (count < 10)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                print("space key was pressed");
+                scoreText.GetComponent<Text>().text = " " + count.ToString();
+            }
+
+
+
+        }
+
+
+    }
+}
